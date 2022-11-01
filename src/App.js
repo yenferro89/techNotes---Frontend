fc270e1fd7f674316771860a4eea3b7bc1a042f2
Yen Pinero Ferro <ypine022@fiu.edu>
2022-11-01 18:25:15 -0400

    commit on "components"

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
-
+import { Route, Routes } from "react-router-dom";
+import Layout from "./components/Layout";
+import Public from "./components/Public";
+import Login from "./features/auth/Login";
+import DashLayout from "./components/DashLayout";
+import Welcome from "./features/auth/Welcome";
+import UsersList from "./features/users/UsersList";
+import NotesList from "./features/notes/NotesList";
 
 function App() {
   return (
-    <div className="h-screen">
-    </div>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Public />} />
+        <Route path="login" element={<Login />} />
+        <Route path="dash" element={<DashLayout />}>
+          <Route index element={<Welcome/>}/>
+          <Route path="notes">
+            <Route index element={<NotesList/>}/>
+          </Route>
+          <Route path="users">
+            <Route index element={<UsersList/>}/>
+          </Route>
+        </Route>
+      </Route>
+    </Routes>
   );
 }
 
