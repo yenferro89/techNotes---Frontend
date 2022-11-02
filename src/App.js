d54d1842820e7e7855b430ecd8e4fb57ab70b05f
Yen Pinero Ferro <ypine022@fiu.edu>
2022-11-02 16:16:33 -0400

    commit on "App"

--- src/App.js
+++ src/App.js
@@ -9,21 +9,23 @@ import NotesList from "./features/notes/NotesList";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Public />} />
-        <Route path="login" element={<Login />} />
-        <Route path="dash" element={<DashLayout />}>
-          <Route index element={<Welcome/>}/>
-          <Route path="notes">
-            <Route index element={<NotesList/>}/>
-          </Route>
-          <Route path="users">
-            <Route index element={<UsersList/>}/>
+    <div className="min-height flex flex-col flex-nowrap">
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Public />} />
+          <Route path="login" element={<Login />} />
+          <Route path="dash" element={<DashLayout />}>
+            <Route index element={<Welcome/>}/>
+            <Route path="notes">
+              <Route index element={<NotesList/>}/>
+            </Route>
+            <Route path="users">
+              <Route index element={<UsersList/>}/>
+            </Route>
           </Route>
         </Route>
-      </Route>
-    </Routes>
+      </Routes>
+    </div>
   );
 }
 
