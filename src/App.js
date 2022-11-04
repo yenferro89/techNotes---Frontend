2454e09146ab41dcab97f0d2dfbe99124be0ddee
Yen Pinero Ferro <ypine022@fiu.edu>
2022-11-04 16:42:09 -0400

    commit on "several components"

--- src/App.js
+++ src/App.js
@@ -1,31 +1,44 @@
-import { Route, Routes } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 import Public from "./components/Public";
 import Login from "./features/auth/Login";
 import DashLayout from "./components/DashLayout";
 import Welcome from "./features/auth/Welcome";
-import UsersList from "./features/users/UsersList";
 import NotesList from "./features/notes/NotesList";
+import UsersList from "./features/users/UsersList";
+import EditUser from "./features/users/EditUser";
+import NewUserForm from "./features/users/NewUserForm";
+import EditNote from "./features/notes/EditNote";
+import NewNote from "./features/notes/NewNote";
+import Prefetch from "./features/auth/Prefetch";
 
 function App() {
   return (
-    <div className="min-height flex flex-col flex-nowrap">
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Public />} />
-          <Route path="login" element={<Login />} />
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Public />} />
+        <Route path="login" element={<Login />} />
+
+        <Route element={<Prefetch />}>
           <Route path="dash" element={<DashLayout />}>
-            <Route index element={<Welcome/>}/>
-            <Route path="notes">
-              <Route index element={<NotesList/>}/>
-            </Route>
+            <Route index element={<Welcome />} />
+
             <Route path="users">
-              <Route index element={<UsersList/>}/>
+              <Route index element={<UsersList />} />
+              <Route path=":id" element={<EditUser />} />
+              <Route path="new" element={<NewUserForm />} />
+            </Route>
+
+            <Route path="notes">
+              <Route index element={<NotesList />} />
+              <Route path=":id" element={<EditNote />} />
+              <Route path="new" element={<NewNote />} />
             </Route>
           </Route>
+          {/* End Dash */}
         </Route>
-      </Routes>
-    </div>
+      </Route>
+    </Routes>
   );
 }
 
