9bc9bbfd738149f218033976b56b76961987258f
Yen Pinero Ferro <ypine022@fiu.edu>
2022-11-10 17:48:31 -0500

    commit on "DashHeader, Login, NewNote"

--- src/features/notes/NewNote.js
+++ src/features/notes/NewNote.js
@@ -1,12 +1,14 @@
-import { useSelector } from "react-redux";
-import { selectAllUsers } from "../users/usersApiSlice";
-import NewNoteForm from "./NewNoteForm";
+import { useSelector } from 'react-redux'
+import { selectAllUsers } from '../users/usersApiSlice'
+import NewNoteForm from './NewNoteForm'
 
 const NewNote = () => {
-  const users = useSelector(selectAllUsers);
+    const users = useSelector(selectAllUsers)
 
-  const content = users ? <NewNoteForm users={users} /> : <p>Loading...</p>;
+    if (!users?.length) return <p>Not Currently Available</p>
 
-  return content;
-};
-export default NewNote;
+    const content = <NewNoteForm users={users} />
+
+    return content
+}
+export default NewNote
