0f0f8ff9ffde681132b4b28d92e9901cfd6b7460
Yen Pinero Ferro <ypine022@fiu.edu>
2022-11-11 14:40:51 -0500

    commit on "App"

--- src/App.js
+++ src/App.js
@@ -14,8 +14,11 @@ import Prefetch from "./features/auth/PreFetch";
 import PersistLogin from "./features/auth/PersistLogin";
 import RequireAuth from "./features/auth/RequireAuth";
 import { ROLES } from "./config/roles";
+import useTitle from "./hooks/useTitle";
 
 function App() {
+  useTitle("Tech Co.");
+
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
